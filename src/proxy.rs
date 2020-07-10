1ed6462446c9dd788af10c912ae4496eda440712
Sean Olson <olson.sean.k@gmail.com>
2020-07-09 23:28:45 -0700

    Implement operations for proxy types.

--- src/proxy.rs
+++ src/proxy.rs
@@ -1,5 +1,7 @@
 use std::marker::PhantomData;
-use std::ops::Add;
+use std::ops::{
+    Add, AddAssign, Div, DivAssign, Mul, MulAssign, Neg, Rem, RemAssign, Sub, SubAssign,
+};
 
 pub trait Constraint<K, T> {
     fn map(inner: T) -> Option<T>;
@@ -77,6 +79,28 @@ where
     }
 }
 
+impl<K, T, C> AddAssign for Proxy<K, T, C>
+where
+    Self: Clone,
+    T: Add<Output = T>,
+    C: Constraint<K, T>,
+{
+    fn add_assign(&mut self, other: Self) {
+        *self = self.clone() + other;
+    }
+}
+
+impl<K, T, C> AddAssign<T> for Proxy<K, T, C>
+where
+    Self: Clone,
+    T: Add<Output = T>,
+    C: Constraint<K, T>,
+{
+    fn add_assign(&mut self, y: T) {
+        *self = self.clone() + y;
+    }
+}
+
 impl<K, T, C> AsRef<T> for Proxy<K, T, C>
 where
     C: Constraint<K, T>,
@@ -103,14 +127,210 @@ where
 {
 }
 
+impl<K, T, C> Div for Proxy<K, T, C>
+where
+    T: Div<Output = T>,
+    C: Constraint<K, T>,
+{
+    type Output = Self;
+
+    fn div(self, other: Self) -> Self::Output {
+        self.zip_map(other, |x, y| x / y)
+    }
+}
+
+impl<K, T, C> Div<T> for Proxy<K, T, C>
+where
+    T: Div<Output = T>,
+    C: Constraint<K, T>,
+{
+    type Output = Self;
+
+    fn div(self, y: T) -> Self::Output {
+        self.map(|x| x / y)
+    }
+}
+
+impl<K, T, C> DivAssign for Proxy<K, T, C>
+where
+    Self: Clone,
+    T: Div<Output = T>,
+    C: Constraint<K, T>,
+{
+    fn div_assign(&mut self, other: Self) {
+        *self = self.clone() / other;
+    }
+}
+
+impl<K, T, C> DivAssign<T> for Proxy<K, T, C>
+where
+    Self: Clone,
+    T: Div<Output = T>,
+    C: Constraint<K, T>,
+{
+    fn div_assign(&mut self, y: T) {
+        *self = self.clone() / y;
+    }
+}
+
 impl<K, T, C> From<T> for Proxy<K, T, C>
 where
     C: Constraint<K, T>,
 {
     fn from(inner: T) -> Self {
         Proxy {
-            inner: C::map(inner).expect(""),
+            inner: C::map(inner).expect("proxy constraint violated"),
             phantom: PhantomData,
         }
+    }
+}
+
+impl<K, T, C> Mul for Proxy<K, T, C>
+where
+    T: Mul<Output = T>,
+    C: Constraint<K, T>,
+{
+    type Output = Self;
+
+    fn mul(self, other: Self) -> Self::Output {
+        self.zip_map(other, |x, y| x * y)
+    }
+}
+
+impl<K, T, C> Mul<T> for Proxy<K, T, C>
+where
+    T: Mul<Output = T>,
+    C: Constraint<K, T>,
+{
+    type Output = Self;
+
+    fn mul(self, y: T) -> Self::Output {
+        self.map(|x| x * y)
+    }
+}
+
+impl<K, T, C> MulAssign for Proxy<K, T, C>
+where
+    Self: Clone,
+    T: Mul<Output = T>,
+    C: Constraint<K, T>,
+{
+    fn mul_assign(&mut self, other: Self) {
+        *self = self.clone() * other;
+    }
+}
+
+impl<K, T, C> MulAssign<T> for Proxy<K, T, C>
+where
+    Self: Clone,
+    T: Mul<Output = T>,
+    C: Constraint<K, T>,
+{
+    fn mul_assign(&mut self, y: T) {
+        *self = self.clone() * y;
+    }
+}
+
+impl<K, T, C> Neg for Proxy<K, T, C>
+where
+    T: Neg<Output = T>,
+    C: Constraint<K, T>,
+{
+    type Output = Self;
+
+    fn neg(self) -> Self::Output {
+        self.map(|x| -x)
+    }
+}
+
+impl<K, T, C> Rem for Proxy<K, T, C>
+where
+    T: Rem<Output = T>,
+    C: Constraint<K, T>,
+{
+    type Output = Self;
+
+    fn rem(self, other: Self) -> Self::Output {
+        self.zip_map(other, |x, y| x % y)
+    }
+}
+
+impl<K, T, C> Rem<T> for Proxy<K, T, C>
+where
+    T: Rem<Output = T>,
+    C: Constraint<K, T>,
+{
+    type Output = Self;
+
+    fn rem(self, y: T) -> Self::Output {
+        self.map(|x| x % y)
+    }
+}
+
+impl<K, T, C> RemAssign for Proxy<K, T, C>
+where
+    Self: Clone,
+    T: Rem<Output = T>,
+    C: Constraint<K, T>,
+{
+    fn rem_assign(&mut self, other: Self) {
+        *self = self.clone() % other;
+    }
+}
+
+impl<K, T, C> RemAssign<T> for Proxy<K, T, C>
+where
+    Self: Clone,
+    T: Rem<Output = T>,
+    C: Constraint<K, T>,
+{
+    fn rem_assign(&mut self, y: T) {
+        *self = self.clone() % y;
+    }
+}
+
+impl<K, T, C> Sub for Proxy<K, T, C>
+where
+    T: Sub<Output = T>,
+    C: Constraint<K, T>,
+{
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self::Output {
+        self.zip_map(other, |x, y| x - y)
+    }
+}
+
+impl<K, T, C> Sub<T> for Proxy<K, T, C>
+where
+    T: Sub<Output = T>,
+    C: Constraint<K, T>,
+{
+    type Output = Self;
+
+    fn sub(self, y: T) -> Self::Output {
+        self.map(|x| x - y)
+    }
+}
+
+impl<K, T, C> SubAssign for Proxy<K, T, C>
+where
+    Self: Clone,
+    T: Sub<Output = T>,
+    C: Constraint<K, T>,
+{
+    fn sub_assign(&mut self, other: Self) {
+        *self = self.clone() - other;
+    }
+}
+
+impl<K, T, C> SubAssign<T> for Proxy<K, T, C>
+where
+    Self: Clone,
+    T: Sub<Output = T>,
+    C: Constraint<K, T>,
+{
+    fn sub_assign(&mut self, y: T) {
+        *self = self.clone() - y;
     }
 }
